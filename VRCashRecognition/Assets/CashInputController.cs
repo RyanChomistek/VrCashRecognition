b956e03c94146f9485732bf52b285e858b727f97
Chomistek <ryan.chomistek@tamu.edu>
2018-11-09 16:36:18 -0600

    adding

---
+++ VRCashRecognition/Assets/CashInputController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class CashInputController : MonoBehaviour {
+
+    public int currentAmount;
+    public TextMeshPro text;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        var currency = other.GetComponent<Currency>();
+        if (currency)
+        {
+            currentAmount += currency.Amount;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        var currency = other.GetComponent<Currency>();
+        if (currency)
+        {
+            currentAmount -= currency.Amount;
+        }
+    }
+}
---
+++ VRCashRecognition/Assets/CashMachineController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class CashMachineController : MonoBehaviour {
+
+    public int targetAmount = 6;
+    public CashInputController CashInput;
+    public TextMeshPro AmountLeftText;
+
+    public void Update()
+    {
+        AmountLeftText.text = $"{targetAmount - CashInput.currentAmount}";
+    }
+
+    public void OnCheck()
+    {
+        //check to see if they have the correct amount
+        //then spit out change
+    }
+}
