73346dd4109b0b0061a69ed8519748ac1c37e2bd
ryan-chomistek <ryan.chomistek@tamu.edu>
2018-11-29 17:38:30 -0600

    asd

---
+++ VRCashRecognition/Assets/ReadTMP.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class ReadTMP : ReadText
+{
+
+    public TextMeshPro text;
+
+    public override string GetText()
+    {
+        return text.text;
+    }
+}
